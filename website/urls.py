432f62fa382af6ab84e897a59d2fd5c11fc4b9c3
LobarBadalova <Lobar.fayzullayeva@mail.ru>
2020-08-25 10:48:23 +0300

    Template is installed, customized. Some minor changes in contact page

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 urlpatterns = [
-	path('', views.home, name='home')    
-]
+	path('', views.home, name='home'),
+	path('contact/', views.contact, name='contact'),   
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- website/views.py
+++ website/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html', {})
+	return render(request, 'index.html', {})
+def contact(request):
+	return render(request, 'contact.html', {})
